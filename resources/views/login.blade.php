bddd60ca2d47038f8e4ce3fd7e736c5eb923c7d9
chicco <s1092913@studenti.univpm.it>
2022-05-15 18:55:41 +0200

    completata registrazione

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,24 +8,24 @@
 <form action="">
 <div class="row justify-content-center">
 <h1 class="login-title"> LOGIN </h1>
-    <div class="col-lg-12 justify-content-center">
+    <div class="col-lg-12 d-flex justify-content-center m-2">
             <label for="username" class="form-label"> Username </label>
             <input type="username" id="username" placeholder="inserisci il tuo username" require>
-</div>
+    </div>
   <!-- Password input -->
-  <div class="col-lg-12 justify-content-center">
+  <div class="col-lg-12 d-flex justify-content-center m-2">
             <label for="password" class="form-label"> Password </label>
             <input type="password" id="password" placeholder="inserisci la tua password" require>
-</div>
+    </div>
   <!-- Submit button -->
   <div class="col-lg-12 ">
-  <button type="button" class="btn login-button btn-primary btn-block mb-4" >Login</button>
+  <button type="button" class="btn d-flex justify-content-center login-button btn-primary btn-block m-3" >Login</button>
+</div>
+<div class="col-lg-12 ">
+    <h7 class ="Link-registrati"><a href="{{ route('register') }}">Non sei registrato?</a></h7>
 </div>
 </div>
 </form>
-<div class="row justify-content-center">
-    <h7 class ="Link-registrati"><a href="{{ route('register') }}">Non sei registrato?</a></h7>
-</div>
 </div>
 
 
