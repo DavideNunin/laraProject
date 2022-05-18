2ee74052256b6eb1ee7e6feca248ff437ffe1036
DavideNunin <davide.nunin@gmail.com>
2022-05-18 12:35:18 +0200

    IL CAROUSEL MO SCI CHE FUNZIONA

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -1,26 +1,26 @@
- <div class="row justify-content-center">
-        <div class="col-sm-12">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
+<div class="row justify-content-center">
+    <div class="col-sm-12">
+        <div id="carouselExampleControls{{$offerta->id}}" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
 
-                @foreach($offerta->get_foto_offerta() as $nomefile) 
-                    <div class="carousel-item @if( $loop->iteration == 1) active @endif">
-                        <img class="d-block w-100 img-carousel" src="{{ asset('images/' . $nomefile->nome_file) }}" alt="Foto offerta">
-                    </div>
-                @endforeach
-
+            @foreach($offerta->get_foto_offerta() as $nomefile) 
+                <div class="carousel-item @if( $loop->iteration == 1) active @endif">
+                    <img class="d-block w-100 img-carousel" src="{{ asset('images/' . $nomefile->nome_file) }}" alt="Foto offerta">
                 </div>
-
-
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-                </a>
+            @endforeach
 
             </div>
+
+
+        <a class="carousel-control-prev" href="#carouselExampleControls{{$offerta->id}}" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+            <a class="carousel-control-next" href="#carouselExampleControls{{$offerta->id}}" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
         </div>
- </div>
+    </div>
+</div>
