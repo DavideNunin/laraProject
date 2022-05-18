2ac6227893af014667733275c58433295ecf8e6a
chicco <s1092913@studenti.univpm.it>
2022-05-18 11:50:26 +0200

    update publiccontroller

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Catalog;
 use App\Models\ElencoFaq;
+use App\Models\Resources\Utente;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\LocatoreController;
 
@@ -11,10 +12,12 @@ class PublicController extends Controller {
 
     protected $_catalogModel;
     protected $_faqModel;
+    protected $_userModel;
 
     public function __construct() {
         $this->_catalogModel = new Catalog;
         $this->_faqModel = new ElencoFaq;
+        $this->_userModel = new Utente;
     }
 
     public function showHomeUser1() {
@@ -61,13 +64,12 @@ class PublicController extends Controller {
             ->with('elfaq', $elfaq);
     }
 
-    public function offerteLocatore(){
-        $catalogo_offerte = $this->_catalogModel->getAll();
+    public function offerte_user_2(){
+        $catalogo_offerte = $this->_userModel->get_offerte_utente();
         $user_type = 1;
 
-        return view('offerte')
+        return view('offertelocatore')
                         ->with('catalogo', $catalogo_offerte)
                         ->with('type_user', $user_type);
     }
-
 }
