8505b1092bbb8b5c0aa55ca230cf41ae687f2558
DennisPierantozzi <s1093366@studenti.univpm.it>
2022-06-02 15:17:04 +0200

    chat now work correctly

--- app/Http/Requests/sendMessageRequest.php
+++ app/Http/Requests/sendMessageRequest.php
@@ -26,6 +26,7 @@ class sendMessageRequest extends FormRequest
     {
         return [
            'messaggio' => 'required',
+           'destinatario' => 'required',
         ];
     }
 }
--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -17,6 +17,7 @@ $(function () {
         $(this).toggleClass("active-chat");
         $(this).find("a:last").css('color', '#f9f9f9');
         id_talking = $(this).attr("id");
+        console.log(id_talking);
         startChat(id_talking);
     });
 
@@ -27,10 +28,10 @@ $(function () {
     var addUrl = "{{ route('chat.send') }}";
     $("#sendMessage-form").on('submit', function (event) {
         event.preventDefault();
-        //if(id_talking == ''){ $("#errMessaggio").text("Seleziona una chat");}
-        //else if($("#messaggio").val() == '') {
-            //$("#errMessaggio").text("Devi scrivere un messaggio");}
-            if(false){}
+        console.log(id_talking);
+        if(id_talking == undefined){ $("#errMessaggio").text("Seleziona una chat");}
+        else if($("#messaggio").val() == '') {
+            $("#errMessaggio").text("Devi scrivere un messaggio");}
         else{sendMessage(addUrl, id_talking);}
     });
 });
@@ -45,6 +46,7 @@ $(function () {
         <div class="row min-vh-100 flex-column flex-md-row">
             
             <aside class="col-12 col-md-2 p-0 flex-shrink-1">
+                <span class="row justify-content-center chat-subtitle"> Le tue chat </span>
                 <nav class="navbar navbar-expand flex-md-column flex-row align-items-center py-2 pl-3">
                     <div class="collapse navbar-collapse ">
                         <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
@@ -58,12 +60,12 @@ $(function () {
                     </div>
                 </nav>
             </aside>
-            <main class="col bg-faded py-3 flex-grow-1">
+            <main class="col bg-faded flex-grow-1" id="main-chat">
                 
                 <div class="container" id="container-message">
                     <!-- Messages with jQuery -->
-                    
                 </div>
+
                 <div class="footer-message fixed-bottom">
                     <div class="container d-flex justify-content-center">
                         {{ Form::open(array('id' => 'sendMessage-form', 'class' => 'Messageform')) }}
@@ -71,7 +73,7 @@ $(function () {
                             <span id="errMessaggio"></span>
                         </div>
                         <div class="col-lg-12">
-                            {{ Form::textarea('messaggio', '', ['class' => 'form-textarea', 'id' => 'messaggio', 'rows' => 3, 'maxlength' => '2000']) }}
+                            {{ Form::textarea('messaggio', '', ['class' => 'form-textarea form-control mb-2', 'id' => 'messaggio', 'rows' => 3, 'maxlength' => '2000']) }}
                         </div>
                         <div class="col-lg-12">
                             {{ Form::button('Invia <i class="fa-solid fa-circle-arrow-right" aria-hidden="true"></i>', ['class' => 'button-form mb-4', 'id' => 'sendForm-submit', 'type' => 'submit']) }}
